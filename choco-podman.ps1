1d8aca99d05243731e0f311b24766ff3da0d4169
Paul Jones <chickenjones44@gmail.com>
2020-02-05 15:07:36 -0600

    Create choco-podman.ps1

---
+++ choco-podman.ps1
@@ -0,0 +1,14 @@
+$installVBox = "choco install virtualbox -y"
+$installPodman = "choco install podman-machine -y"
+$testchoco = powershell choco -v
+
+if(-not($testchoco)){
+    Write-Output "Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else{
+    Write-Output "Chocolatey Version $testchoco is already installed"
+}
+
+iex $installVBox
+iex $installPodman
